ef9e9b4895197b657dbfb651af617683bbc1bb41
paulinenatasha <s4027616@student.rmit.edu.au>
2025-05-07 00:42:43 +1000

    experiments
    the circles still won't appear and there were no sounds played

--- dms4/echoscape/script.js
+++ dms4/echoscape/script.js
@@ -1,38 +1,44 @@
-const synth = new Tone.Synth().toDestination();
-let audioStarted = false;
+window.scale = ["C3", "D3", "E3", "G3", "A3", "C4", "D4", "E4", "G4", "A4"];
 
-//Canvas
-const stage = new Konva.Stage({
-  container: "container",
-  width: window.innerWidth,
-  height: window.innerHeight,
+window.addEventListener("DOMContentLoaded", () => {
+  synth = new Tone.Synth().toDestination();
+
+  //Canvas
+  stage = new Konva.Stage({
+    container: "container",
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  layer = new Konva.Layer();
+  stage.add(layer);
 });
 
-const layer = new Konva.Layer();
-stage.add(layer);
+let synth;
+let audioStarted = false;
+let stage;
+let layer;
 
 //Modal function
-document.getElementById("dialogBtn").addEventListener("click", () => {
-  document.getElementById("introDialog").close();
-  toneInit();
-});
 document.getElementById("introDialog").showModal();
+document.getElementById("dialogBtn").addEventListener("click", async () => {
+  await Tone.start();
+  audioStarted = true;
+  document.getElementById("introDialog").close();
+});
 
 //Tone.js function
-function toneInit() {
-  Tone.start().then(() => {
-    audioStarted = true;
-  });
+function getNoteFromPosition(x) {
+  const screenWidth = window.innerWidth;
+  const noteIndex = Math.floor((x / screenWidth) * (window.scale.lenght - 1));
+  return window.scaale[Math.min(noteIndex, window.scale.length - 1)];
 }
 
 stage.on("tap contentClick", (e) => {
   if (!audioStarted) return;
-
   const pos = stage.getPointerPosition();
-
   createCircle(pos.x, pos.y);
-
-  playNote();
+  playNote(pos.x);
 });
 
 //Create shapes (only circles for now, will expand more)
@@ -42,6 +48,7 @@ function createCircle(x, y) {
     y: y,
     radius: 20,
     fill: "rgb(249, 174, 123)",
+    opacity: 0.8,
   });
 
   layer.add(circle);
@@ -59,6 +66,6 @@ function createCircle(x, y) {
 }
 
 // Play note function
-function playNote() {
-  synth.triggerAttackRelease("C4", "8n");
+function playNote(x) {
+  synth.triggerAttackRelease(note, "8n");
 }
